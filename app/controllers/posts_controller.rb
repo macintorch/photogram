23aa7f527d1063255ccae3719747569c27c5f44c
Ainor Syahrizal <macintorch@gmail.com>
2016-03-07 16:26:43 +0800

    can upload image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
 	def index
-		
+		@posts = Post.all
 	end
 
 	def new
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.create(post_params)
+		byebug
 		redirect_to posts_path
 	end
 
