198bdfce313f2490445374d23fb6f8bb105fe298
Ainor Syahrizal <macintorch@gmail.com>
2016-03-10 15:57:38 +0800

    comments paginated

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'posts#index'
+   get ':user_name', to 'profiles#show', as: :profile
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
