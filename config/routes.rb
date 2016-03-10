7de3fe29e5c3fd00f96f91a98ef70715e24b6335
Ainor Syahrizal <macintorch@gmail.com>
2016-03-10 16:43:17 +0800

    bio profile page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'posts#index'
    get ':user_name', to: 'profiles#show', as: :profile
-
+   get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+   patch ':user_name/edit', to: 'profiles#update', as: :update_profile
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
