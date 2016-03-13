240d22e5a0345d9d4870b462486df389cb048b5d
Ainor Syahrizal <macintorch@gmail.com>
2016-03-13 21:10:19 +0800

    user can follow other user

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
   	has_many :comments, dependent: :destroy
   	validates :caption, length: { minimum: 3, maximum: 300 }
   	has_many :notifications, dependent: :destroy
+  	scope :of_followed_users, -> (following_users) { where user_id: following_users }
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
